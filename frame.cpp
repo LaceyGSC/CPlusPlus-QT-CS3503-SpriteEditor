fe9bff11e8cd333a7b6bd2dfeee5a20ae1b0745d
stevewreese <stephenw.reese@gmail.com>
2016-10-25 16:03:46 -0600

    added frame to gui

--- frame.cpp
+++ frame.cpp
@@ -3,8 +3,8 @@
 Frame::Frame(int width, int height): QImage(width, height, QImage::Format_ARGB32){
     //WIDTH = width;
     //HEIGHT = height;
-    int opacity = 255;// Set this between 0 and 255
-    image->fill(QColor(0,0,0,opacity));
+    //int opacity = 255;// Set this between 0 and 255
+    //image->fill(QColor(0,0,0,opacity));
 
 
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,11 +10,19 @@ MainWindow::MainWindow(QWidget *parent) :
     QGraphicsView* view;
     Frame firstFrame = project.getFrame(0);
     QPixmap image = QPixmap::fromImage(firstFrame);
+    int opacity = 255;// Set this between 0 is transparent
+    image.fill(QColor(255,0,0,opacity));
+
     canvas = new QGraphicsScene(this);
     canvas ->addPixmap(image);
     canvas->setSceneRect(image.rect());
+
+    QBrush brush(QColor(0, 0, 0, 150));
+    canvas->setBackgroundBrush(brush);
+
     view = new QGraphicsView(canvas);
-    setCentralWidget(view);
+
+    ui->drawingGridLayout->addWidget(view);
 }
 
 MainWindow::~MainWindow()
